4aa32aba96600d4a4abca08993acf31d837d48d2
Edward Wilson <edward.wilson@live.com>
2015-11-02 10:42:50 +0000

    Added permission check to reset

--- scripts/restart-hubot.js
+++ scripts/restart-hubot.js
@@ -18,8 +18,13 @@
 
   module.exports = function (robot) {
     robot.respond(/restart( yourself)?$/i, function (msg) {
-      msg.send("Ok; restarting now...");
-      process.exit(1);
+      if (robot.auth.hasRole(msg.envelope.user, 'admin')) {
+        msg.send("Ok; restarting now...");
+        process.exit(1);
+      }
+      else {
+        msg.reply("Sorry you can't ask that, you're not an admin");
+      }
     });
   };
 } ());
